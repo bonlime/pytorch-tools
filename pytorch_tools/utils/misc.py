ac0304e5e6b1a5585c8b8c5f1627749a91a963b3
Emil Zakirov <bonlimezak@gmail.com>
2019-08-13 13:29:12 +0300

    add misc

---
+++ pytorch_tools/utils/misc.py
@@ -0,0 +1,12 @@
+import torch.nn.functional as F
+from functools import partial
+
+def activation_from_name(act_name, act_param=0.01):
+    if act_name == 'relu':
+        return partial(F.relu, inplace=True)
+    elif act_name == 'leaky_relu'
+        return partial(F.leaky_relu, negative_slope=act_param, inplace=True)
+    elif act_name == "elu":
+        return partial(F.elu, alpha=act_param, inplace=True)
+    elif act_name == "identity":
+        return lambda x: x
