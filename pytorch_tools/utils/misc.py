4d3e273c447a7d67bcec3e2be9db2f9b6ca408d8
Emil Zakirov <bonlimezak@gmail.com>
2019-08-13 14:46:48 +0300

    typo

--- pytorch_tools/utils/misc.py
+++ pytorch_tools/utils/misc.py
@@ -4,7 +4,7 @@ from functools import partial
 def activation_from_name(act_name, act_param=0.01):
     if act_name == 'relu':
         return partial(F.relu, inplace=True)
-    elif act_name == 'leaky_relu'
+    elif act_name == 'leaky_relu':
         return partial(F.leaky_relu, negative_slope=act_param, inplace=True)
     elif act_name == "elu":
         return partial(F.elu, alpha=act_param, inplace=True)
