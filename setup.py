c80a8331852767a37676954a95663fab8e40bcd4
Emil Zakirov <bonlimezak@gmail.com>
2019-08-14 15:25:47 +0300

    add requirements

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 with open("README.md") as f:
     readme = f.read()
 
-#with open("requirements.txt") as f:
-#    requirements = f.read().split()
+with open("requirements.txt") as f:
+   requirements = f.read().split()
 
 setup(name="pytorch_tools",
       version="0.0.1",
@@ -14,6 +14,16 @@ setup(name="pytorch_tools",
       url="https://github.com/bonlime/pytorch-tools",
       description="Tool box for PyTorch",
       long_description=readme,
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+      ],
+      # requirements
+      setup_requires=["setuptools_scm"],
+      python_requires=">=3, <4",
+      install_requires=requirements,
+
       #license="Apache License 2.0",
-      #install_requires=requirements,
+      #
       )
