1418f08f3c722a205f6ee3e5e77b60d87ec1df45
Emil Zakirov <bonlimezak@gmail.com>
2019-08-16 16:07:18 +0300

    add balanced_acc

--- pytorch_tools/metrics/accuracy.py
+++ pytorch_tools/metrics/accuracy.py
@@ -1,5 +1,3 @@
-
-
 class Accuracy:
 
     def __init__(self, topk=1):
@@ -7,16 +5,15 @@ class Accuracy:
         self.topk = topk
     
     def __call__(self, output, target):
-        # TODO maybe can write it even shorter
         _, pred = output.topk(self.topk, 1, True, True)
         pred = pred.t()
         correct = pred.eq(target.view(1, -1).expand_as(pred))
         correct_k = correct[:self.topk].view(-1).float().sum(0, keepdim=True)
         return  correct_k.mul(100.0 / output.size(0))
 
-
 class BalancedAccuracy:
-    """y_true = [0, 1, 0, 0, 1, 0]
+    """ BalancedAccuracy == mean of recalls for each class
+        y_true = [0, 1, 0, 0, 1, 0]
         >>> y_pred = [0, 1, 0, 0, 0, 1]
         >>> BalancedAccuracy()(y_true, y_pred)
         0.625
@@ -25,4 +22,13 @@ class BalancedAccuracy:
         self.name = 'BalancedAcc'
 
     def __call__(self, output, target):
-        raise NotImplementedError
+        # if raw preds, then argmax them
+        if output.shape != target.shape:
+            output = output.argmax(1)
+        correct = output.eq(target)
+        result = 0
+        for cls in target.unique():
+            tp = (correct * target.eq(cls)).sum().float()
+            tp_fn = target.eq(cls).sum()
+            result += tp / tp_fn
+        return result.mul(100. / target.unique().size(0))
