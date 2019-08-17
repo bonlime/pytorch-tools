fcdd6f2abbb95220f1cd21916524f4d81d0e3a51
Emil Zakirov <bonlimezak@gmail.com>
2019-08-17 13:25:08 +0300

    draft

--- pytorch_tools/fit_wrapper/wrapper.py
+++ pytorch_tools/fit_wrapper/wrapper.py
@@ -8,11 +8,21 @@ from torch import nn
 from apex import amp
 
 
-def get_val(metrics):
-    results = [(m.name, m.avg()) for m in metrics]
-    names, vals = list(zip(*results))
-    out = ['{} : {:4f}'.format(name, val) for name, val in results]
-    return vals, ' | '.join(out)
+
+# fit_generator
+#   _run_batch_train
+# predict_generator
+# evaluate_generator
+
+# В керасе есть
+# train on batch
+# test on batch
+# predict on batch
+# когда надо сделать val, вызывает evalute_generator
+# в eval считаем всякие лоссы. 
+# в pred только прогоняем вход через сетку и стакаем, ничего не храним
+
+
 
 
 class FitWrapper(nn.Module):
@@ -20,10 +30,10 @@ class FitWrapper(nn.Module):
         super(FitWrapper, self).__init__()
         self.model = model
 
-    def compile(self, opt, criterion, metrics):
-        self.opt = optimizer
+    def compile(self, optimizer, criterion, metrics=None):
+        self.optimizer = optimizer
         self.criterion = criterion
-        self.metrics = metrics
+        self.metrics = metrics or [] #maybe need to listify
 
     def _fit_epoch(generator, steps_per_epoch=None):
         timer = TimeMeter()
@@ -38,19 +48,22 @@ class FitWrapper(nn.Module):
             output = self.model(batch)
             loss = self.criterion(model)
             # compute grads
-            self.opt.zero_grad()
-            with amp.scale_loss(loss, optimizer) as scaled_loss:
+            self.optimizer.zero_grad()
+            with amp.scale_loss(loss, self.optimizer) as scaled_loss:
                 scaled_loss.backward()
-            optimizer.step()
+            self.optimizer.step()
+            # calc metrics
+            for metric, meter in zip(self.metrics, metric_meters):
+                    meter.update(to_numpy(metric(output, target)))
+            loss_meter.update(to_numpy(loss))
             # sync
             torch.cuda.synchronize()
             timer.batch_end()
             bs = batch.size(0)
-            loss_meter.update(loss)
-            out = ['{} : {:4f}'.format(m.name, mm.avg_smooth) for m, mm in zip(self.metrics, metric_meters)]
-            desc = 'Loss: {}'.format(loss_meter.avg_smooth) + ' | '.join(out)
+            out = self._format_meters(metric_meters)
+            desc = 'Loss: {} |'.format(loss_meter.avg_smooth) + out
             pbar.set_description(desc)
-        return loss_meter.avg # + return something else
+        return loss_meter.avg, [m.avg for m in metric_meters]
 
     def fit_generator(generator, 
                       steps_per_epoch=None, # not yet supported
@@ -63,39 +76,43 @@ class FitWrapper(nn.Module):
 
         # do something
         for ep in range(epochs):
-            self._fit_epoch(generator)
-            
+            # callbacks.on_epoch_begin
+            train_loss, train_m = self._fit_epoch(generator)
             if val_generator and ep % val_freq:
-                val_loss, val_m  = self.evaluate_generator(val_generator, val_steps)
-                    
+                val_loss, val_m  = self.evaluate_generator(val_generator, val_steps, use_pbar=False)
+            # callbacks.on_epoch_end
+        # callbacks.on_train_end
 
-    #TODO add predict_generator
+    #TODO add predict_generators
 
-    def evaluate_generator(self, generator, *, steps=None, return_pred=False):
+    def evaluate_generator(self, generator, steps=None, *, use_pbar=True):
+        # eval metrics on generator
         timer = TimeMeter()
         loss_meter = AverageMeter()
         metric_meters [AverageMeter() for i in self.metrics]
         model.eval()
-        preds_list = []
-        pbar = tqdm(generator, ascii=True)
+        pbar = tqdm(generator, ascii=True) if use_pbar else generator
         for batch, target in pbar:
             timer.batch_start()
             with torch.no_grad():
                 output = self.model(batch)
-                loss = self.criterion(output, target).data
+                loss = self.criterion(output, target).data #do we need data?
+                loss_meter.update(loss)
                 for metric, meter in zip(self.metrics, metric_meters):
                     meter.update(metric(output, target))
-            bs = batch.size(0)
-            loss_meter.update(loss)
-            if return_pred:
-                preds_list.append(to_numpy(output))
+            #bs = batch.size(0)
             torch.cuda.synchronize()
             timer.batch_end()
             # TODO add info from timer
-            out = ['{} : {:4f}'.format(m.name, mm.avg_smooth) for m, mm in zip(self.metrics, metric_meters)]
-            desc = 'Loss: {}'.format(loss_meter.avg_smooth) + ' | '.join(out)
-            pbar.set_description(desc)
-        if return_pred:
-            return loss_meter.avg, [m.avg for m in metric_meters], preds_list
-        else:
-            return loss_meter.avg, [m.avg for m in metric_meters]
+            out = self._format_meters(metric_meters)
+            desc = 'Loss: {} |'.format(loss_meter.avg_smooth) + out
+            if use_pbar:
+                pbar.set_description(desc)
+        
+        return loss_meter.avg, [m.avg for m in metric_meters]
+
+    def _format_meters(self, meters):
+        # meters: instances of AverageMeter
+        results = [(m.name, meter.avg_smooth) for m, meter in zip(self.metrics, meters)]
+        out = ['{} : {:4f}'.format(name, val) for name, val in results]
+        return ' | '.join(out)
