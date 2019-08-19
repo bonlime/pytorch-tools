937ba6b33b210edfeb0752908a54fe0099ba1c21
zakajd <djamilzak@gmail.com>
2019-08-19 16:23:45 +0300

    added doc string

--- pytorch_tools/optim/radam.py
+++ pytorch_tools/optim/radam.py
@@ -2,14 +2,14 @@ import math
 import torch
 from torch.optim.optimizer import Optimizer, required
 
-"""
-Clone of https://github.com/LiyuanLucasLiu/RAdam repo
-Keep it updated with original repo for future improvements
-https://arxiv.org/pdf/1908.03265.pdf
 
-"""
 class RAdam(Optimizer):
-
+    """
+    Clone of https://github.com/LiyuanLucasLiu/RAdam repo
+    Keep it updated with original repo for future improvements
+    https://arxiv.org/pdf/1908.03265.pdf
+    fp16 training is NOT supported, use fp32 instead
+    """
     def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8, weight_decay=0):
         defaults = dict(lr=lr, betas=betas, eps=eps, weight_decay=weight_decay)
         self.buffer = [[None, None, None] for ind in range(10)]
