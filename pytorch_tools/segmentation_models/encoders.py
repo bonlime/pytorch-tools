30fc42adf9250302a4f1cee2d5cee7135836e924
Emil Zakirov <bonlimezak@gmail.com>
2019-08-19 13:15:31 +0300

    fix encoders

--- pytorch_tools/segmentation_models/encoders.py
+++ pytorch_tools/segmentation_models/encoders.py
@@ -1,10 +1,29 @@
 import pytorch_tools.models as models
 
-encoders = {}
-encoders.update(vgg_encoders)
-encoders.update(resnet_encoders)
-encoders.update(resnext_encoders)
-encoders.update(senet_encoders)
+ENCODER_SHAPES = {
+    'vgg11_bn': (512, 512, 512, 256, 128),
+    'vgg13_bn': (512, 512, 512, 256, 128),
+    'vgg16_bn': (512, 512, 512, 256, 128),
+    'vgg19_bn': (512, 512, 512, 256, 128),
+
+    'resnet18': (512, 256, 128, 64, 64),
+    'resnet34': (512, 256, 128, 64, 64),
+    'resnet50': (2048, 1024, 512, 256, 64),
+    'resnet101': (2048, 1024, 512, 256, 64),
+    'resnet152': (2048, 1024, 512, 256, 64),
+
+    'se_resnet34': (512, 256, 128, 64, 64),
+    'se_resnet50': (2048, 1024, 512, 256, 64),
+    'se_resnet101': (2048, 1024, 512, 256, 64),
+    'se_resnet152': (2048, 1024, 512, 256, 64),
+    'se_resnext50_32x4d': (2048, 1024, 512, 256, 64),
+    'se_resnext101_32x4d': (2048, 1024, 512, 256, 64),
+
+    'densenet121': (1024, 1024, 512, 256, 64),
+    'densenet169': (1664, 1280, 512, 256, 64),
+    'densenet201': (1920, 1792, 512, 256, 64),
+    #'densenet161': (2208, 2112, 768, 384, 96),
+}
 
 
 
@@ -12,6 +31,9 @@ def get_encoder(name, **kwargs):
     if name not in models.__dict__:
         raise ValueError('No such encoder: {}'.format(name))
     kwargs['encoder'] = True
+    #if 'resne' in name:
+    #    kwargs['dilated'] = True # dilate resnets for better performance
     kwargs['pretrained'] = kwargs.pop('encoder_weights')
     m = models.__dict__[name](**kwargs)
+    m.out_shapes = ENCODER_SHAPES[name]
     return m
