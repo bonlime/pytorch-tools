9ef56beebdba4ef77d27dd86fce816f6066c7e83
zakajd <djamilzak@gmail.com>
2019-08-23 02:48:13 +0300

    bug fix

--- pytorch_tools/losses/vgg_loss.py
+++ pytorch_tools/losses/vgg_loss.py
@@ -4,26 +4,6 @@ It's work in progress, no guarantees that code will work
 """
 from pytorch_tools import models
 import torch.nn as nn
-LossOutput = namedtuple("LossOutput", ["relu1_2", "relu2_2", "relu3_3", "relu4_3"])
-
-class LossNetwork(torch.nn.Module):
-    def __init__(self, vgg_model):
-        super(LossNetwork, self).__init__()
-        self.vgg_layers = vgg_model.features
-        self.layer_name_mapping = {
-            '3': "relu1_2",
-            '8': "relu2_2",
-            '15': "relu3_3",
-            '22': "relu4_3"
-        }
-    
-    def forward(self, x):
-        output = {}
-        for name, module in self.vgg_layers._modules.items():
-            x = module(x)
-            if name in self.layer_name_mapping:
-                output[self.layer_name_mapping[name]] = x
-        return LossOutput(**output)
 
 class ContentLoss(_Loss):
     """
