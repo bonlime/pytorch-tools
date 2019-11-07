0a93eb48ec8c68add02cc9cba7fdf6a203c88362
Emil Zakirov <bonlimezak@gmail.com>
2019-11-07 15:01:48 +0000

    fix CCE

--- pytorch_tools/losses/smooth.py
+++ pytorch_tools/losses/smooth.py
@@ -4,7 +4,7 @@ from .base import Loss
 class CrossEntropyLoss(Loss):
     """CE with optional smoothing and support for multiple positive labels. Supports only one reduction for now
     Args:
-        one_hot (bool): set to True if targets are already one_hot
+        one_hot (bool): set to False if you pass LongTensor as targets
         num_classes (int): number of classes. only used if one_hot is False
     """
     def __init__(self, smoothing = 0.0, one_hot=True, num_classes=None):
@@ -13,26 +13,21 @@ class CrossEntropyLoss(Loss):
         self.smoothing = smoothing
         if not one_hot and num_classes == None:
             raise ValueError('To convert data to one_hot you have to specify num_classes')
-        self.one_hot = torch.Tensor([one_hot])
         self.num_classes = num_classes
 
     def forward(self, x, target):
-        if self.training:
-            if self.one_hot:
-                target_one_hot = target.float()
-            else:
-                target_one_hot = torch.zeros(target.size(0), self.num_classes, 
-                                             dtype=torch.float, device=x.device)
-                target_one_hot.scatter_(1, target.unsqueeze(1), 1.0)
-            x = x.float()
-            logprobs = torch.nn.functional.log_softmax(x, dim = -1)
-            # mupltiple labels 
-            nll_loss = -logprobs * target_one_hot
-            nll_loss = nll_loss.sum(-1)
-            smooth_loss = -logprobs.mean(dim=-1)
-            loss = self.confidence * nll_loss + self.smoothing * smooth_loss
-            return loss.mean()
+        # check is explicit on purpose, don't rely on self.one_hot
+        if len(target.shape) == 2:
+            target_one_hot = target.float()
         else:
-            if self.one_hot:
-                target = target.argmax(-1)
-            return torch.nn.functional.cross_entropy(x, target)
+            target_one_hot = torch.zeros(target.size(0), self.num_classes, 
+                                            dtype=torch.float, device=x.device)
+            target_one_hot.scatter_(1, target.unsqueeze(1), 1.0)
+        x = x.float()
+        logprobs = torch.nn.functional.log_softmax(x, dim = -1)
+        # mupltiple labels handling
+        nll_loss = -logprobs * target_one_hot
+        nll_loss = nll_loss.sum(-1)
+        smooth_loss = -logprobs.mean(dim=-1)
+        loss = self.confidence * nll_loss + self.smoothing * smooth_loss
+        return loss.mean()
