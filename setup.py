28b91a672a0b7ee3ddb06988d94c5c3647b471c6
Emil Zakirov <bonlimezak@gmail.com>
2019-12-11 18:23:50 +0000

    fix setup slightly

--- setup.py
+++ setup.py
@@ -8,22 +8,20 @@ with open("requirements.txt") as f:
 
 setup(name="pytorch_tools",
       version="0.0.1",
-      author="bonlime",
+      author="Emil Zakirov",
       author_email="bonlimezak@gmail.com",
       packages=find_packages(exclude=["test", "docs", "examples"]),
       url="https://github.com/bonlime/pytorch-tools",
       description="Tool box for PyTorch",
       long_description=readme,
       classifiers=[
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
       ],
-      # requirements
       setup_requires=["setuptools_scm"],
       python_requires=">=3, <4",
-      install_requires=requirements,
-
-      #license="Apache License 2.0",
-      #
+      install_requires=[
+        requirements,
+        'inplace-abn @ git+https://github.com/mapillary/inplace_abn.git@v1.0.8',
+      ],
+      license="MIT License",
       )
