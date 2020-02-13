c7dde020be7c0818f7d469ca3f0dd490e56cd5fa
Emil Zakirov <bonlimezak@gmail.com>
2020-02-13 21:46:19 +0300

    bump

--- setup.py
+++ setup.py
@@ -18,9 +18,9 @@ setup(
     classifiers=["Programming Language :: Python :: 3.6",],
     setup_requires=["setuptools_scm"],
     python_requires=">=3, <4",
-    install_requires=[
-        requirements,
-        "inplace-abn @ git+https://github.com/mapillary/inplace_abn.git@v1.0.11",
+    install_requires=requirements,
+    dependency_links=[
+        'https://github.com/mapillary/inplace_abn.git#egg=inplace_abn-1.0.11',
     ],
     license="MIT License",
 )
