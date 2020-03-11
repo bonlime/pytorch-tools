f8071547789f81fceaafb920a55aef871baba57d
Emil Zakirov <bonlimezak@gmail.com>
2020-03-11 14:47:53 +0300

    add proper versioning

--- pytorch_tools/__init__.py
+++ pytorch_tools/__init__.py
@@ -1,3 +1,5 @@
+__version__ = "0.1.0"
+
 from . import fit_wrapper
 from . import losses
 from . import metrics
--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import re
 from setuptools import setup, find_packages
 
 with open("README.md") as f:
@@ -6,9 +7,13 @@ with open("README.md") as f:
 with open("requirements.txt") as f:
     requirements = f.read().split()
 
+with open("pytorch_tools/__init__.py") as f:
+    VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+    version = re.search(VSRE, f.read()).group(1)
+
 setup(
     name="pytorch_tools",
-    version="0.0.1",
+    version=version,
     author="Emil Zakirov",
     author_email="bonlimezak@gmail.com",
     packages=find_packages(exclude=["test", "docs", "examples"]),
