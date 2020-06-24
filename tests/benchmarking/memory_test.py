14f6dd72b92d0476cf5197000afc1c8aece4c1dc
zakajd <djamilzak@gmail.com>
2020-06-24 11:09:02 +0300

    minor

--- tests/benchmarking/memory_test.py
+++ tests/benchmarking/memory_test.py
@@ -11,6 +11,8 @@ import torch.backends.cudnn as cudnn
 import pytorch_tools as pt
 from pytorch_tools import models
 import pytorch_tools.segmentation_models as pt_sm
+import effdet
+import timm
 
 
 class AverageMeter:
@@ -24,8 +26,10 @@ class AverageMeter:
         self.count += 1
         self.avg = self.sum / self.count
 
+
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters())
+
 
 @pytest.mark.skip("Not meant for pytest")
 def test_model(model, forward_only=False):
@@ -39,8 +43,8 @@ def test_model(model, forward_only=False):
 
         def run_once():
             start.record()
-            output = model(INP)
-            if isinstance(output, tuple) and len(output) > 1:
+            output = model(INP, TARGET) if TARGET is not None else model(INP)
+            if isinstance(output, (list, tuple)):
                 output = output[0]
             f_end.record()
             if forward_only:
@@ -89,13 +93,34 @@ def test_model(model, forward_only=False):
     del model
 
 
+import torch.nn as nn
+
+
+class DetectionTrainWrapper(nn.Module):
+    def __init__(self, modell, size):
+        super().__init__()
+        self.loss = None
+        self.model = modell
+        anchors = pt.utils.box.generate_anchors_boxes(size)[0]
+        # self.loss = pt.losses.DetectionLoss(anchors)
+        self.loss = torch.jit.script(pt.losses.DetectionLoss(anchors))
+
+    def forward(self, inp, target):
+        # cls_out, box_out = self.model(inp)
+        # return box_out
+        loss = self.loss(self.model(inp), target)
+        return loss
+        # return box_out
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Model Benchmarking")
     parser.add_argument(
         "--forward", "-f", action="store_true", help="Flag to only run forward. Disables grads"
     )
+    parser.add_argument("--amp", action="store_true", help="Measure speed using apex mixed precision")
     parser.add_argument(
-        "--amp", action="store_true", help="Measure speed using apex mixed precision"
+        "--torch_amp", action="store_true", help="Measure speed using torch native mixed precision"
     )
     parser.add_argument(
         "--bs", default=64, type=int, help="BS for benchmarking",
@@ -106,52 +131,39 @@ if __name__ == "__main__":
     args = parser.parse_args()
     # all models are first init to cpu memory to find errors earlier
     models_dict = {
-        # 'VGG16 ABN' : models.vgg16_bn(norm_layer='abn'),
-        # 'VGG 16 InplaceABN:': models.vgg16_bn(norm_layer='inplaceabn'),
-        # 'Resnet50 OS 32:' : models.resnet50(deep_stem=True, antialias=True, output_stride=32), #norm_layer='abn',
-        # 'Resnet50 OS 16:': models.resnet50(deep_stem=True, antialias=True, output_stride=16),
-        # "Resnet50:": models.resnet50(),
-        # "Se-Resnet50:": models.resnet50(use_se=True),
-        # "Resnet50 D:": models.resnet50(deep_stem=True, antialias=True),  # norm_layer='inplaceabn', ),
-        # "Se-Resnet50 D": models.resnet50(deep_stem=True, antialias=True, use_se=True),
-        # 'Resnet50 OS 8 InPlace:': models.resnet50(deep_stem=True, antialias=True, output_stride=8, norm_layer='inplaceabn', norm_act='leaky_relu'),
-        'Resnet50': models.resnet50(),
-        'Resnet50 tv': tv.models.resnet50(),
-        # 'Resnet50 D Antialias:': models.resnet50(antialias=True),
-        # 'Resnet50 Inplace': models.resnet50(norm_act="leaky_relu", norm_layer="inplaceabn")
-        # 'Resnet50 ABN:' : models.se_resnet50(norm_layer='abn'),
-        # 'Resnet50 InPlaceABN:': models.se_resnet50(norm_layer='inplaceabn', norm_act='leaky_relu'),
-        # 'Densenet121 MemEff' : models.densenet121(memory_efficient=True),
-        # 'Densenet121 NotMemEff' : models.densenet121(memory_efficient=False),
-        # "Densenet121 ABN": models.densenet121(norm_layer="abn", norm_act="leaky_relu"),
-        # "Densenet121 InPlaceABN": models.densenet121(norm_layer="inplaceabn", norm_act="leaky_relu"),
-        # 'SE Resnext50_32x4 ABN:' : models.se_resnext50_32x4d(norm_layer='abn'),
-        # 'SE Resnext50_32x4 InplaceABN:' : models.se_resnext50_32x4d(norm_layer='inplaceabn')
+        # "EffDet0 My": pt.detection_models.efficientdet_d0(match_tf_same_padding=False, pretrained=None),
+        # "EffDet0 My Wrapped": DetectionTrainWrapper(
+        #     modell=pt.detection_models.efficientdet_d0(match_tf_same_padding=False, pretrained=None),
+        #     size=args.sz,
+        # )
+        "EffB0": pt.models.efficientnet_b0(),
+        # "ResNet50": pt.models.resnet50(),
     }
     segm_models_dict = {
         # "Unet Resnet34": pt_sm.Unet("resnet34"),
-        # "Unet SEResnet50": pt_sm.Unet("se_resnet50"),
-        # "Linknet SEResnet50": pt_sm.Linknet("se_resnet50"),
-        # "Segm BiFPN SEResnet50": pt_sm.SegmentationBiFPN("se_resnet50"),
-        # "Segm FPN SEResnet50": pt_sm.SegmentationFPN("se_resnet50"),
-        # "Deeplab SEResnet50": pt_sm.DeepLabV3("se_resnet50"),
-        # "Unet SEResnet50": pt_sm.Unet("se_resnet50"),
     }
 
     print("Initialized models")
     BS = args.bs
     SZ = args.sz
-    N_RUNS = 10
+    N_RUNS = 5
     RUN_ITERS = 10
     INP = torch.ones((BS, 3, SZ, SZ), requires_grad=not args.forward).cuda(0)
+    TARGET = None
+    # TARGET = torch.randint(0, 50, (BS, 20, 5)).cuda().float()
+
     for name, model in models_dict.items():
         print(f"{name} {count_parameters(model) / 1e6:.2f}M params")
         model = model.cuda(0)
         if args.amp:
             model = amp.initialize(model, verbosity=0, opt_level="O1")
             INP = INP.half()
+        if args.torch_amp:
+            model.forward = torch.cuda.amp.autocast()(model.forward)
         if args.forward:
             model.eval()
+
+        # with torch.cuda.amp.autocast(enabled=args.torch_amp):
         test_model(model, forward_only=args.forward)
 
     # now test segmentation models
@@ -160,7 +172,9 @@ if __name__ == "__main__":
     for name, model in segm_models_dict.items():
         enc_params = count_parameters(model.encoder) / 1e6
         total_params = count_parameters(model) / 1e6
-        print(f"{name}. Encoder {enc_params:.2f}M. Decoder {total_params - enc_params:.2f}M. Total {total_params:.2f}M params")
+        print(
+            f"{name}. Encoder {enc_params:.2f}M. Decoder {total_params - enc_params:.2f}M. Total {total_params:.2f}M params"
+        )
         model = model.cuda(0)
         if args.amp:
             model = amp.initialize(model, verbosity=0, opt_level="O1")
