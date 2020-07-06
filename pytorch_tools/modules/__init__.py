0c1c59cf3220e359250fd928469b8cff72e7575b
zakajd <djamilzak@gmail.com>
2020-07-06 18:05:33 +0300

    add SyncABN

--- pytorch_tools/modules/__init__.py
+++ pytorch_tools/modules/__init__.py
@@ -21,6 +21,7 @@ from .activations import activation_from_name
 from .activations import Mish, MishNaive, Swish, SwishNaive
 
 from .activated_batch_norm import ABN
+from .activated_batch_norm import SyncABN
 from .activated_group_norm import AGN
 from .activated_no_norm import NoNormAct
 from inplace_abn import InPlaceABN, InPlaceABNSync
@@ -30,12 +31,17 @@ def bn_from_name(norm_name):
     norm_name = norm_name.lower()
     if norm_name == "abn":
         return ABN
+    elif norm_name in ("syncabn", "sync_abn", "abn_sync"):
+        return SyncABN
     elif norm_name in ("inplaceabn", "inplace_abn"):
         return InPlaceABN
     elif norm_name == "inplaceabnsync":
         return InPlaceABNSync
     elif norm_name in ("frozen_abn", "frozenabn"):
         return partial(ABN, frozen=True)
+    # not sure anyone would ever need this but let's support just in case
+    elif norm_name == "frozen_sync_abn":
+        return partial(SyncABN, frozen=True)
     elif norm_name in ("agn", "groupnorm", "group_norm"):
         return AGN
     elif norm_name in ("none",):
