aac4544568a2513615668503db3fd01559badb9e
zakajd <djamilzak@gmail.com>
2020-07-30 13:13:40 +0300

    add BatchOverfit Callback

--- pytorch_tools/fit_wrapper/callbacks.py
+++ pytorch_tools/fit_wrapper/callbacks.py
@@ -1,6 +1,5 @@
 import os
 import math
-import logging
 import numpy as np
 from tqdm import tqdm
 from enum import Enum
@@ -826,3 +825,26 @@ class ModelEma(Callback):
             return
         # return model back
         self.state.model = self.model_copy
+
+
+class BatchOverfit(Callback):
+    """Remembers first batch and tries to overfit it. Useful for debug.
+    NOTE: Should go after all other callbacks to make sure it's the last thing to change the input
+    Args:
+        save_batch (bool): If True will save first batch. Useful for visualization"""
+
+    def __init__(self, save_batch=False):
+        super().__init__()
+        self.has_saved = False
+        self.save_batch = save_batch
+        self.batch = None
+
+    def on_batch_begin(self):
+        if not self.has_saved:
+            self.has_saved = True
+            self.batch = self.state.input[0].clone(), self.state.input[1].clone()
+            if self.save_batch:
+                torch.save(self.batch[0], "b_img")
+                torch.save(self.batch[1], "b_target")
+        else:
+            self.state.input = self.batch
