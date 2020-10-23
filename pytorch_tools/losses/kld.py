843e0610e331202b45df87d7a0d3942b97b2864b
zakajd <djamilzak@gmail.com>
2020-10-23 18:02:29 +0300

    add ignore value to KLD return momentum back to default value

--- pytorch_tools/losses/kld.py
+++ pytorch_tools/losses/kld.py
@@ -26,9 +26,11 @@ class BinaryKLDivLoss(Loss):
         from_logits (bool): If False assumes sigmoid has already been applied to model output
         smoothing (float): if not None, clamps prediction and target to avoid too large values of loss
             should be in [0, 1]
+        ignore_label (None or int): If not None, targets may contain values to be ignored.
+            Target values equal to ignore_label will be ignored from loss computation.
     """
 
-    def __init__(self, reduction="mean", from_logits=True, smoothing=1e-6):
+    def __init__(self, reduction="mean", from_logits=True, smoothing=1e-6, ignore_label=-1):
         super().__init__()
         self.reduction = Reduction(reduction)
         self.from_logits = from_logits
@@ -36,19 +38,21 @@ class BinaryKLDivLoss(Loss):
         self.prob_clamp = smoothing
         # get value for clamping logits by inverse of sigmoid
         self.logit_clamp = torch.tensor(smoothing / (1 - smoothing)).log().item()
+        self.ignore_label = ignore_label
 
     def forward(self, y_pred, y_true):
         # clamp to avoid nan's and to achieve smoothign effect
-        y_true = torch.clamp(y_true, self.prob_clamp, 1 - self.prob_clamp)
+        y_true_clamped = torch.clamp(y_true, self.prob_clamp, 1 - self.prob_clamp)
         y_pred = torch.clamp(y_pred, self.logit_clamp, 1 - self.logit_clamp)
-
-        # squeeze to allow different shapes like BSx1xHxW vs BSxHxW
         if self.from_logits:
-            bce_loss = F.binary_cross_entropy_with_logits(y_pred, y_true, reduction="none")
+            bce_loss = F.binary_cross_entropy_with_logits(y_pred, y_true_clamped, reduction="none")
         else:
-            bce_loss = F.binary_cross_entropy(y_pred, y_true, reduction="none")
-        bce_min = y_true * y_true.log() + (1 - y_true) * (1 - y_true).log()
+            bce_loss = F.binary_cross_entropy(y_pred, y_true_clamped, reduction="none")
+        bce_min = y_true * y_true_clamped.log() + (1 - y_true) * (1 - y_true_clamped).log()
         kld_loss = bce_loss + bce_min  # bce is negative so need to sum
+        # account for ignore value
+        not_ignored = y_true.ne(self.ignore_label)
+        kld_loss = kld_loss.where(not_ignored, torch.zeros(1).to(kld_loss))
         if self.reduction == Reduction.MEAN:
             kld_loss = kld_loss.mean()
         elif self.reduction == Reduction.SUM:
